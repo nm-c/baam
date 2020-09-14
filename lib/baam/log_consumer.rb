7bd0bb9d54c5587148eb7c4cc879d7aa4ada83af
nextmark <nextmark.co@gmail.com>
2020-09-14 23:46:45 +0900

    Use unique queue name

--- lib/baam/log_consumer.rb
+++ lib/baam/log_consumer.rb
@@ -13,7 +13,6 @@ module Baam
 
     def initialize(logger)
       @logger = logger
-      @name = @logger.class.name
     end
 
     def log_impl(body, _info, _prop)
@@ -24,7 +23,7 @@ module Baam
       Thread.start do
         connection.with_channel do |channel|
           block ||= method(:log_impl)
-          queue = channel.queue(@name)
+          queue = channel.queue('', auto_delete: true)
           exchange = channel.topic(LogQueue::EXCHANGE_NAME)
           queue.bind(exchange, routing_key: '#')
           queue.subscribe(block: true) do |info, prop, body|
